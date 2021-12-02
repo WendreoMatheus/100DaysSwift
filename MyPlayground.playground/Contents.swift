f7a4f7aa56ff76587caaa1dde85ebc938f8e0399
Wendreo Matheus <wendreo.pvh@gmail.com>
2021-12-02 16:45:32 -0300

    Inicial Commit

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,44 @@
+import UIKit
+
+// Strings and Integers
+
+var str = "Hello, playground"
+var age = 38
+var population = 8_000_000
+
+
+
+// Multline Strings
+var str1 = """
+This goes \
+over multiple \
+lines
+"""
+
+var str2 = """
+This goes \
+over multiple \
+lines
+"""
+
+// Doubles and Booleans
+
+var pi = 3.141
+var awesome = true
+
+// String Interpolation
+
+var score = 85
+var message = "Your score was \(score)"
+var results = "The test results are here: \(message)"
+
+
+// Constants
+let taylor = "swift"
+
+
+// Type notations
+let album: String = "Reputation"
+let year: Int = 1989
+let height: Double = 1.78
+let taylorRocks: Bool = true
